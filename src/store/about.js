b583962e9f9925552337ea7f4b8dbe6c22fb6e3d
suminmer <ennisdelma@naver.com>
2022-06-09 19:59:53 +0900

    Logo path modified again

--- src/store/about.js
+++ src/store/about.js
@@ -5,6 +5,6 @@ export default {
     email: 'metinthesummer@naver.com',
     blog: 'https://github.com/Suminmer',
     phone: '+82-10-1234-5678',
-    image: 'assets/SummerCat.png'
+    image: 'https://raw.githubusercontent.com/Suminmer/vue-movie-app/main/src/assets/SummerCat.png'
   })
 }
